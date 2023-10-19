2c4aea487fd33b6f7b2a5578c3dce52325ba32e5
victor-eidt <victoreidtrl@gmail.com>
2023-10-19 20:53:20 -0300

    10 métodos. 10 atributos.
    Adicionei mais métodos e atributos para a classe abstrata do produto, ela possui 8 agora no total. Fiz dessa forma para que as classes possam herdar esses atributos/métodos genéricos, e adicionar mais 2 específicos dependendo da necessidade de cada tipo de produto.

--- Produto.java
+++ Produto.java
@@ -3,6 +3,10 @@ abstract class Produto implements IProduto {
     private String descricao;
     private double preco;
     private String categoria;
+    private boolean isFragil;
+    private double peso;
+    private String marca;
+    private String cor;
 
     public Produto(String nome, String descricao, double preco, String categoria) {
         this.nome = nome;
@@ -21,7 +25,21 @@ abstract class Produto implements IProduto {
     public void setPreco(double preco) { this.preco = preco; }
 
     public String getCategoria() { return categoria; }
-    public void setCategoria(String categoria) { this.categoria = categoria; }
+    public void setCategoria(String categoria) {this.categoria = categoria;}
+    public double calcularFrete() {
+        return 0;
+    }
+    public void aplicarDesconto(double percentual) {
+
+    }
+
+    public boolean verificarDisponibilidadeEmEstoque() {
+        return false;
+    }
+
+    public void registrarSaidaDeEstoque(int quantidade) {
+
+    }
 
     public void exibirInformacoes() {
         System.out.println("Nome: " + nome + ", Descrição: " + descricao + ", Preço: " + preco + ", Categoria: " + categoria);
